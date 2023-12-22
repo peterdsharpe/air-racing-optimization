f3d4703a9b2b6e452dfa7a1671c7193bf4414e17
Peter Sharpe <pds@mit.edu>
2023-12-22 16:39:22 +0100

    add timers

--- terrain_model/compare_models.py
+++ terrain_model/compare_models.py
@@ -3,8 +3,9 @@ from fourier_model import get_elevation_fourier_north_east, get_elevation_fourie
 from load_raw_data import terrain_data
 import numpy as np
 import casadi as cas
+from aerosandbox.tools.code_benchmarking import Timer
 
-N = 1000
+N = 200
 
 lat_query = np.linspace(
     47,
@@ -17,8 +18,8 @@ lon_query = np.linspace(
     N
 )
 
-lat_query = cas.MX(lat_query)
-lon_query = cas.MX(lon_query)
+# lat_query = cas.MX(lat_query)
+# lon_query = cas.MX(lon_query)
 
 import matplotlib.pyplot as plt
 import aerosandbox.tools.pretty_plots as p
@@ -30,28 +31,31 @@ fig, ax = plt.subplots()
 #
 # interpolate.
 
-print("Printing interpolated data...")
-elev_interpolated = get_elevation_interpolated_lat_lon(
-    lat_query,
-    lon_query,
-    resolution=(300, 100)
-)
-elev_interpolated = np.array(cas.evalf(elev_interpolated))
+with Timer("Interpolation"):
+    elev_interpolated = get_elevation_interpolated_lat_lon(
+        lat_query,
+        lon_query,
+        resolution=(100, 300)
+    )
+    elev_interpolated = np.array(cas.evalf(elev_interpolated))
 plt.plot(
     np.array(cas.evalf(lon_query)),
     elev_interpolated,
     label="Interpolated",
 )
 
-# print("Printing Fourier data...")
-# elev_fourier = get_elevation_fourier_lat_lon(
-#     lat_query,
-#     lon_query,
-# )
-# plt.plot(
-#     lon_query,
-#     elev_fourier,
-#     label="Fourier",
-# )
+with Timer("Fourier"):
+    elev_fourier = get_elevation_fourier_lat_lon(
+        lat_query,
+        lon_query,
+        resolution=(1000, 1000)
+    )
+plt.plot(
+    lon_query,
+    elev_fourier,
+    label="Fourier",
+)
+
+print("Done")
 
 p.show_plot()
