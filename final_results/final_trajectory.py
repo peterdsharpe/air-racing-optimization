3025aebaf04c81aeb84112c0d3544508077bde26
Peter Sharpe <pds@mit.edu>
2024-01-17 01:00:08 -0500

    Add final results

--- final_results/final_trajectory.py
+++ final_results/final_trajectory.py
@@ -9,55 +9,43 @@ dyn: asb.DynamicsPointMass3DSpeedGammaTrack = asb.load(
     "./dyn.asb"
 )
 
-time: np.ndarray = np.load("./time.npy")
+time: np.ndarray = dyn.other_fields["time"]
 
-speedup_over_realtime = 1
-video_fps = 60
-
-frame_time = speedup_over_realtime / video_fps
-
-### Resample
-time_video = np.arange(0, time[-1], frame_time)
-from scipy import interpolate
-
-state_interpolators = {
-    k: interpolate.InterpolatedUnivariateSpline(
-        x=time,
-        y=v,
-    )
-    for k, v in dyn.state.items()
-}
-control_interpolators = {
-    k: interpolate.InterpolatedUnivariateSpline(
-        x=time,
-        y=v,
-    )
-    for k, v in dyn.control_variables.items()
-}
-dyn = dyn.get_new_instance_with_state({
-    k: v(time_video)
-    for k, v in state_interpolators.items()
-})
-for k, v in control_interpolators.items():
-    setattr(dyn, k, v(time_video))
-
-# dyn = dyn.get_new_instance_with_state(
-#     {
-#         k: interpolate.interp1d(
-#             time,
-#             v,
-#             kind="cubic"
-#         )(time_video)
-#         for k, v in dyn.state.items()
-#     }
-# )
-
+# speedup_over_realtime = 1
+# video_fps = 60
+#
+# frame_time = speedup_over_realtime / video_fps
+#
+# ### Resample
+# time_video = np.arange(0, time[-1], frame_time)
+# from scipy import interpolate
+#
+# state_interpolators = {
+#     k: interpolate.InterpolatedUnivariateSpline(
+#         x=time,
+#         y=v,
+#     )
+#     for k, v in dyn.state.items()
+# }
+# control_interpolators = {
+#     k: interpolate.InterpolatedUnivariateSpline(
+#         x=time,
+#         y=v,
+#     )
+#     for k, v in dyn.control_variables.items()
+# }
+# dyn = dyn.get_new_instance_with_state({
+#     k: v(time_video)
+#     for k, v in state_interpolators.items()
+# })
+# for k, v in control_interpolators.items():
+#     setattr(dyn, k, v(time_video))
 
 N = len(dyn)
 
 
 fig, ax = plt.subplots(
-    figsize=(16, 6)
+    figsize=(11, 5)
 )
 plt.plot(
     dyn.y_e,
@@ -81,51 +69,132 @@ plt.imshow(
     alpha=1,
     zorder=2
 )
+plt.xlabel("Position East of Datum [m]")
+plt.ylabel("Position North of Datum [m]")
+
+from matplotlib import patheffects
+plt.annotate(
+    "Start",
+    xy=(terrain_data_zoomed["east_start"], terrain_data_zoomed["north_start"]),
+    xytext=(0, 0),
+    textcoords="offset points",
+    ha="left",
+    va="bottom",
+    color="k",
+    path_effects=[
+        patheffects.withStroke(
+            linewidth=3,
+            foreground="w",
+        )
+    ]
+)
+plt.annotate(
+    "Finish",
+    xy=(terrain_data_zoomed["east_end"], terrain_data_zoomed["north_end"]),
+    xytext=(0, 0),
+    textcoords="offset points",
+    ha="right",
+    va="top",
+    color="k",
+    path_effects=[
+        patheffects.withStroke(
+            linewidth=3,
+            foreground="w",
+        )
+    ]
+)
+
+plt.colorbar(label="Terrain Elevation [m]")
 p.equal()
 p.show_plot(
-    "3D Continuous Optimization",
+    # "3D Continuous Optimization",
     rotate_axis_labels=False,
     savefig=[
-        # f"./figures/trajectory_{resolution}.svg",
+        f"../figures/trajectory.svg",
     ]
 )
 
 N = len(dyn)
 
 
-plotter = pv.Plotter()
-plotter.window_size = 1920, 1080
-plotter.set_background(
-    color="#FFFFFF",
-    top="#A5B8D7",
-)
-
-dyn.draw(
-    backend="pyvista",
-    plotter=plotter,
-    show=False,
-    n_vehicles_to_draw=int(N / (video_fps / 4)),
-    scale_vehicle_model=14 * 5,
-    # scale_vehicle_model=1,
-    trajectory_line_color="red",
-    draw_global_grid=False,
-    draw_axes=False,
-)
-
-grid = pv.RectilinearGrid(
-    terrain_data_zoomed["north_edges"],
-    terrain_data_zoomed["east_edges"],
-)
-grid["elev"] = terrain_data_zoomed["elev"].T.flatten()
-grid = grid.warp_by_scalar("elev", factor=-1)
-plotter.add_mesh(
-    grid.extract_geometry(),
-    scalars="elev",
-    cmap='terrain',
-    specular=0.5,
-    specular_power=15,
-    smooth_shading=True,
-    show_scalar_bar=False,
-)
-plotter.enable_terrain_style()
-plotter.show()
+# plotter = pv.Plotter()
+# plotter.window_size = 1920, 1080
+# plotter.set_background(
+#     color="#FFFFFF",
+#     top="#A5B8D7",
+# )
+#
+# dyn.draw(
+#     backend="pyvista",
+#     plotter=plotter,
+#     show=False,
+#     n_vehicles_to_draw=N // 100,
+#     scale_vehicle_model=14 * 5,
+#     # scale_vehicle_model=1,
+#     trajectory_line_color="red",
+#     draw_global_grid=False,
+#     draw_axes=False,
+# )
+#
+# grid = pv.RectilinearGrid(
+#     terrain_data_zoomed["north_edges"],
+#     terrain_data_zoomed["east_edges"],
+# )
+# grid["elev"] = np.ravel(terrain_data_zoomed["elev"], order="F")
+#
+# dx, dy = np.gradient(
+#     terrain_data_zoomed["elev"],
+#     terrain_data_zoomed["north_edges"],
+#     terrain_data_zoomed["east_edges"],
+#     edge_order=2,
+#     n=1
+# )
+# ddx, ddy = np.gradient(
+#     terrain_data_zoomed["elev"],
+#     terrain_data_zoomed["north_edges"],
+#     terrain_data_zoomed["east_edges"],
+#     edge_order=2,
+#     n=2
+# )
+# mag = np.sqrt(dx ** 2 + dy ** 2)
+# slope_deg = np.arctan2d(mag, 1)
+#
+# lap = ddx + ddy
+# curvature = np.sqrt(ddx ** 2 + ddy ** 2)
+#
+# def normalize(x, q1=0.1, q2=0.9):
+#     low, med, high = np.quantile(x, [q1, 0.5, q2])
+#     from scipy import interpolate
+#     norm = interpolate.interp1d(
+#         [low, med, high],
+#         [-1, 0, 1],
+#         fill_value="extrapolate",
+#     )(x)
+#     return np.tanh(norm)
+#
+# color = (
+#     np.clip(slope_deg, 0, 30) / 30
+#     - 0.5 * normalize(lap)
+#     + 0.2 * normalize(curvature)
+#     + 0.2 * normalize(terrain_data_zoomed["elev"])
+# )
+# from scipy import ndimage
+#
+# color = ndimage.gaussian_filter(color, sigma=0.5)
+# # color = normalize(color, q1=0.01, q2=0.9)
+#
+# grid["color"] = np.ravel(color, order="F")
+#
+# grid = grid.warp_by_scalar("elev", factor=-1)
+# plotter.add_mesh(
+#     grid.extract_geometry(),
+#     scalars="color",
+#     cmap='terrain',
+#     # cmap=cmocean.cm.topo,
+#     specular=0.5,
+#     specular_power=15,
+#     # smooth_shading=True,
+#     show_scalar_bar=False,
+# )
+# plotter.enable_terrain_style()
+# plotter.show()
--- final_results/final_trajectory_orbit_video.py
+++ final_results/final_trajectory_orbit_video.py
@@ -4,37 +4,21 @@ import matplotlib.pyplot as plt
 import aerosandbox.tools.pretty_plots as p
 import pyvista as pv
 from get_task_info import terrain_data_zoomed
-from scipy import interpolate
 
 dyn: asb.DynamicsPointMass3DSpeedGammaTrack = asb.load(
     "./dyn.asb"
 )
 
-time: np.ndarray = dyn.other_fields["time"]
+time: np.ndarray = np.load("./time.npy")
 
-speedup_over_realtime = 1  # How much faster than realtime to render the video
-video_fps = 30  # Frames per second
-video_fraction = (0.6, 0.7)  # Only render part of the video
+speedup_over_realtime = 1
+video_fps = 60
 
 frame_time = speedup_over_realtime / video_fps
 
-terrain_altitude_interpolator = interpolate.RegularGridInterpolator(
-    points=(
-        terrain_data_zoomed["north_edges"],
-        terrain_data_zoomed["east_edges"],
-    ),
-    values=terrain_data_zoomed["elev"],
-    method="linear",
-    bounds_error=False,
-    fill_value=np.nan,
-)
-
 ### Resample
-time_video = np.arange(
-    time[-1] * video_fraction[0],
-    time[-1] * video_fraction[1],
-    frame_time
-)
+time_video = np.arange(0, time[-1], frame_time)
+from scipy import interpolate
 
 state_interpolators = {
     k: interpolate.InterpolatedUnivariateSpline(
@@ -44,19 +28,11 @@ state_interpolators = {
     for k, v in dyn.state.items()
 }
 control_interpolators = {
-    k: interpolate.PchipInterpolator(
+    k: interpolate.InterpolatedUnivariateSpline(
         x=time,
         y=v,
     )
     for k, v in dyn.control_variables.items()
-}
-other_fields_interpolators = {
-    k: interpolate.PchipInterpolator(
-        x=time,
-        y=v,
-    )
-    for k, v in dyn.other_fields.items()
-    if np.length(v) == np.length(time)
 }
 dyn = dyn.get_new_instance_with_state({
     k: v(time_video)
@@ -64,19 +40,18 @@ dyn = dyn.get_new_instance_with_state({
 })
 for k, v in control_interpolators.items():
     setattr(dyn, k, v(time_video))
-dyn.other_fields = {
-    k: v(time_video)
-    for k, v in other_fields_interpolators.items()
-}
 
-terrain_altitude = terrain_altitude_interpolator(
-    np.stack([
-        dyn.x_e,
-        dyn.y_e,
-    ], axis=1)
-)
+# dyn = dyn.get_new_instance_with_state(
+#     {
+#         k: interpolate.interp1d(
+#             time,
+#             v,
+#             kind="cubic"
+#         )(time_video)
+#         for k, v in dyn.state.items()
+#     }
+# )
 
-altitude_agl = dyn.altitude - terrain_altitude
 
 N = len(dyn)
 
@@ -86,7 +61,6 @@ plotter.set_background(
     color="#FFFFFF",
     top="#A5B8D7",
 )
-plotter.enable_anti_aliasing('ssaa')
 
 dyn.draw(
     backend="pyvista",
@@ -105,63 +79,42 @@ grid = pv.RectilinearGrid(
     terrain_data_zoomed["east_edges"],
 )
 grid["elev"] = terrain_data_zoomed["elev"].T.flatten()
-
-dx, dy = np.gradient(
-    terrain_data_zoomed["elev"],
-    terrain_data_zoomed["north_edges"],
-    terrain_data_zoomed["east_edges"],
-    edge_order=2,
-    n=1
-)
-ddx, ddy = np.gradient(
-    terrain_data_zoomed["elev"],
-    terrain_data_zoomed["north_edges"],
-    terrain_data_zoomed["east_edges"],
-    edge_order=2,
-    n=2
-)
-mag = np.sqrt(dx ** 2 + dy ** 2)
-slope_deg = np.arctan2d(mag, 1)
-
-lap = ddx + ddy
-curvature = np.sqrt(ddx ** 2 + ddy ** 2)
-
-def normalize(x, q1=0.1, q2=0.9):
-    low, med, high = np.quantile(x, [q1, 0.5, q2])
-    from scipy import interpolate
-    norm = interpolate.interp1d(
-        [low, med, high],
-        [-1, 0, 1],
-        fill_value="extrapolate",
-    )(x)
-    return np.tanh(norm)
-
-color = (
-    np.clip(slope_deg, 0, 30) / 30
-    - 0.5 * normalize(lap)
-    + 0.2 * normalize(curvature)
-    + 0.2 * normalize(terrain_data_zoomed["elev"])
-)
-from scipy import ndimage
-
-color = ndimage.gaussian_filter(color, sigma=0.5)
-# color = normalize(color, q1=0.01, q2=0.9)
-
-grid["color"] = np.ravel(color, order="F")
-
 grid = grid.warp_by_scalar("elev", factor=-1)
 plotter.add_mesh(
     grid.extract_geometry(),
-    scalars="color",
+    scalars="elev",
     cmap='terrain',
-    # cmap=cmocean.cm.topo,
     specular=0.5,
     specular_power=15,
-    # smooth_shading=True,
+    smooth_shading=True,
     show_scalar_bar=False,
 )
+# plotter.enable_terrain_style()
 
-plotter.open_movie("trajectory_short.mp4", framerate=video_fps, quality=5)
+# plotter.show(auto_close=False)
+
+# plotter.open_gif("trajectory.gif")
+plotter.open_movie("trajectory.mp4", framerate=video_fps)
+
+# i = 0
+
+
+up_vectors = np.stack(
+    dyn.convert_axes(0, 0, -1, "body", "earth"),
+    axis=1
+)
+
+terrain_altitude_interpolator = interpolate.RegularGridInterpolator(
+    points=(
+        terrain_data_zoomed["north_edges"],
+        terrain_data_zoomed["east_edges"],
+    ),
+    values=terrain_data_zoomed["elev"],
+    method="linear",
+    bounds_error=False,
+    fill_value=np.nan,
+
+)
 
 from tqdm import tqdm
 
@@ -187,22 +140,20 @@ for i in tqdm(range((N - 1))):
     plotter.camera.position = pos
     plotter.camera.focal_point = pos + dir * 10
     plotter.camera.up = (0, 0, -1)
-    plotter.camera.roll = plotter.camera.roll + np.degrees(dyn.bank[i])
-    plotter.camera.view_angle = 90  # FOV
+    plotter.camera.roll = plotter.camera.roll + np.degrees(dyn.bank[i]) * 0.8
+    plotter.camera.view_angle = 90 # FOV
     plotter.camera.clipping_range = (1, 200000)
 
     text = "\n".join([
         f"Time: {time_video[i]:.1f} s",
         f"Airspeed: {dyn.speed[i]:.0f} m/s",
         f"Altitude: {dyn.altitude[i]:.0f} m",
-        f"Altitude AGL: {altitude_agl[i]:.0f} m",
-        # f"AoA: {dyn.alpha[i]:.1f} deg",
-        # f"Throttle: {dyn.other_fields['throttle'][i]:.0%}",
-        f"G-Force: {dyn.other_fields['accel_G'][i]:.1f} G",
+        f"Altitude AGL: {dyn.altitude[i] - terrain_altitude_interpolator((dyn.x_e[i], dyn.y_e[i])):.0f} m",
+        f"AoA: {np.degrees(dyn.alpha[i]):.1f} deg",
         f"Bank: {np.degrees(dyn.bank[i]):.0f} deg",
         f"Gamma: {np.degrees(dyn.gamma[i]):.0f} deg",
         f"Bearing: {np.degrees(dyn.track[i]):.0f} deg",
-    ])
+        ])
 
     plotter.add_text(
         text,
